0e87eee36eb8686813698d9915ece15fd4dab89a
pump-scripts <capybaryco@gmail.com>
2025-02-10 13:22:25 -0800

    Delete pumpfun-commentbot-main directory

--- pumpfun-commentbot-main/index.js
+++
@@ -1,8 +0,0 @@
-import createPumpProfiles from './main.js';
-import chalk from "chalk";
-
-createPumpProfiles().then(() => {
-
-}).catch(error => {
-    console.error(chalk.redBright('An error occurred:', error));
-});
